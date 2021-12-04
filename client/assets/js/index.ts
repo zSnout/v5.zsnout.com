364a5221984774130022bd4d8c73656c831687cf
Zachary <zsakowitz@zsnout.com>
2021-12-04 09:51:17 -0800

    Add beforeinstallprompt listener

--- client/assets/js/index.ts
+++ client/assets/js/index.ts
@@ -1,4 +1,5 @@
 import $ from "./jsx.js";
+import { createNotification } from "./notification.js";
 
 /** Checks the current theme set in `localStorage` and updates the page accordingly. */
 export function checkTheme() {
@@ -24,4 +25,46 @@ declare global {
   }
 }
 
-export {};
+let hasPrompted = false;
+if (
+  !localStorage.didInstall ||
+  (localStorage.didInstall == "false" &&
+    +localStorage.lastInstallTime! < Date.now() - 1000 * 60 * 60 * 24 * 7)
+) {
+  window.addEventListener("beforeinstallprompt", (event: any) => {
+    if (hasPrompted) return;
+    hasPrompted = true;
+    let hide = createNotification(
+      "Did you know you can install this app to your device? If you do, you can use it offline!",
+      20000,
+      {
+        async Install() {
+          hide();
+          event.prompt();
+
+          let { outcome } = await event.userChoice;
+
+          if (outcome == "dismissed") localStorage.didInstall = "false";
+          else localStorage.didInstall = "true";
+
+          localStorage.lastInstallTime = Date.now().toString();
+        },
+        async Cancel() {
+          hide();
+          localStorage.didInstall = "false";
+          localStorage.lastInstallTime = Date.now().toString();
+        },
+      }
+    );
+  });
+}
+
+declare global {
+  interface Storage {
+    /** Whether the user has installed the app. */
+    didInstall?: "true" | "false";
+
+    /** The time of the last install. */
+    lastInstallTime?: string;
+  }
+}
