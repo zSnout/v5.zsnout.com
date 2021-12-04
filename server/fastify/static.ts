1fa7d31c06515be3976e16dba3f30d6050466ed6
Zachary <zsakowitz@zsnout.com>
2021-12-04 08:06:54 -0800

    Update CSP

--- server/fastify/static.ts
+++ server/fastify/static.ts
@@ -3,11 +3,16 @@ import { ServerResponse } from "http";
 import { join } from "path";
 import server from "..";
 
+let csp = `default-src 'self'
+  connect-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com/
+  style-src 'self' https://fonts.googleapis.com
+  font-src 'self' https://fonts.gstatic.com/
+  object-src 'none'
+  base-uri 'none'
+  report-uri /csp`.replaceAll("\n", "; ");
+
 server.addHook("preHandler", (req, res, next) => {
-  res.header(
-    "content-security-policy",
-    "default-src 'self'; style-src 'self' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com/; object-src 'none'; base-uri 'none'; report-uri /csp"
-  );
+  res.header("content-security-policy", csp);
 
   next();
 });
