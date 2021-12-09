6968e25000d8575e755535307e84685f60753aac
Zachary Sakowitz <zsakowitz@zsnout.com>
2021-12-08 17:56:15 -0800

    Add operating instructions & update remote location

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
-git clone https://github.com/zsnout/zsnout.com
+git clone https://github.com/zsnout/zsnout.com.git zsnout
 cd zsnout.com
 npm install
 npm run build
-echo "Completed!"
+echo "
+zSnout has been installed!
+To watch the directory, run 'npm run watch'.
+To build the directory, run 'npm run build'.
+To launch the server, run 'npm start'.
+If you update any files in the 'server/cli' folder make sure to rebuild the project before watching, as these files are used in the watch process.
+For more information, check out the README available at https://github.com/zSnout/zsnout.com/#readme."
