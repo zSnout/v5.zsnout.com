3c83cd6ed71b14aa2123fef856baadfc505b7794
Zachary <zsakowitz@zsnout.com>
2021-12-14 17:56:45 -0800

    Move `edit` to `smtojs`

--- client/storymatic/smtojs/index.ts
+++ client/storymatic/smtojs/index.ts
@@ -2,3 +2,6 @@ import { edit } from "../../assets/js/ace.js";
 
 let editor = edit("editor");
 editor.session.setMode("ace/mode/storymatic");
+
+let jsviewer = edit("jsviewer");
+editor.session.setMode("ace/mode/javascript");
