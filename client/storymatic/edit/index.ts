090abd7f593290dc72944c50a5460847278a2d29
Zachary <zsakowitz@zsnout.com>
2021-12-14 17:41:50 -0800

    Allow overscrolling

--- client/storymatic/edit/index.ts
+++ client/storymatic/edit/index.ts
@@ -1,6 +1,7 @@
 let editor = ace.edit("editor");
 editor.setFontSize("1em");
 editor.setShowPrintMargin(false);
+editor.setOption("scrollPastEnd", true);
 editor.session.setMode("ace/mode/storymatic");
 editor.session.setUseWrapMode(true);
 editor.session.setUseSoftTabs(true);
