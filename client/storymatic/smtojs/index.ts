2a0077bab0459fd9feddffd204fcb7d53dfbf905
Zachary <zsakowitz@zsnout.com>
2021-12-15 12:46:36 -0800

    Fix editor

--- client/storymatic/smtojs/index.ts
+++ client/storymatic/smtojs/index.ts
@@ -1,13 +1,20 @@
 import { edit } from "../../assets/js/ace.js";
 import { storyToJS } from "../lib.js";
 
+let skipChange = false;
+
 function onchange() {
   jsviewer.session.setValue(storyToJS(editor.getValue()));
+  if (skipChange) return;
   localStorage.smToJS = editor.getValue();
 }
 
 window.addEventListener("storage", (ev) => {
-  if (ev.key == "smToJS" && ev.newValue) editor.session.setValue(ev.newValue);
+  if (ev.key == "smToJS" && ev.newValue) {
+    skipChange = true;
+    editor.session.setValue(ev.newValue);
+    skipChange = false;
+  }
 });
 
 let editor = edit("editor");
@@ -17,10 +24,12 @@ let jsviewer = edit("jsviewer");
 jsviewer.session.setMode("ace/mode/javascript");
 jsviewer.setReadOnly(true);
 
-if (localStorage.smToJS) {
-  editor.session.setValue(localStorage.smToJS);
-  onchange();
-}
+localStorage.smToJS =
+  localStorage.smToJS || `"Hello world!"\ndef @myfunc $param\n  # pass`;
+
+editor.session.setValue(localStorage.smToJS);
+onchange();
+
 editor.session.on("change", onchange);
 
 declare global {
