d5b0435a076e01fff908152ee6ffaffc55a92502
Zachary <zsakowitz@zsnout.com>
2021-12-14 17:49:45 -0800

    Add `ace.ts` to create Ace instance

---
+++ client/assets/js/ace.ts
@@ -0,0 +1,22 @@
+import { zQuery } from "./jsx.js";
+
+/**
+ * Creates an Ace editor instance and adds some presets.
+ * @param el The element to initialize an editor in.
+ * @returns An Ace editor instance.
+ */
+export function edit(el: zQuery | HTMLElement | string): AceAjax.Editor {
+  if (el instanceof zQuery) el = el[0];
+
+  let editor = ace.edit(el as any);
+  editor.setFontSize("1em");
+  editor.setShowPrintMargin(false);
+  editor.setOption("scrollPastEnd", true);
+  editor.session.setUseWrapMode(true);
+  editor.session.setUseSoftTabs(true);
+  editor.session.setTabSize(2);
+  editor.container.classList.remove("ace-tm");
+  editor.container.classList.add("cobalt2");
+
+  return editor;
+}
