1788e5a048547ee69c94c2256aeada7e6a842f82
Zachary <zsakowitz@zsnout.com>
2021-12-14 18:08:54 -0800

    Make `jsviewer` update with Storymatic code

--- client/storymatic/smtojs/index.ts
+++ client/storymatic/smtojs/index.ts
@@ -1,7 +1,13 @@
 import { edit } from "../../assets/js/ace.js";
+import { storyToJS } from "../lib.js";
 
 let editor = edit("editor");
 editor.session.setMode("ace/mode/storymatic");
 
 let jsviewer = edit("jsviewer");
-editor.session.setMode("ace/mode/javascript");
+jsviewer.session.setMode("ace/mode/javascript");
+jsviewer.setReadOnly(true);
+
+editor.session.on("change", () => {
+  setTimeout(() => jsviewer.session.setValue(storyToJS(editor.getValue())));
+});
