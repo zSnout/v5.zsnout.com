8606ca9ecebf1a2e20b96f040f5094a7c7bc055e
Zachary <zsakowitz@zsnout.com>
2021-12-22 05:33:05 -0800

    Update gh routes

--- routes/github.ts
+++ routes/github.ts
@@ -2,7 +2,7 @@ import server from "../server";
 
 let repo = "https://github.com/zsnout/zsnout.com";
 
-server.redirect("/gh/", repo);
+server.redirect("/gh", repo);
 server.redirect("/gh/repo", repo);
 server.redirect("/gh/contributors", `${repo}/graphs/contributors`);
 server.redirect("/gh/zsakowitz", "https://github.com/zsakowitz");
