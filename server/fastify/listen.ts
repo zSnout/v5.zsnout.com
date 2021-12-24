6d5f896445b8c5ac018cd72381df20f499f94d8f
Zachary <zsakowitz@zsnout.com>
2021-12-23 19:33:02 -0800

    Fix issue where site was only served on localhost, not 127.0.0.1

--- server/fastify/listen.ts
+++ server/fastify/listen.ts
@@ -1,7 +1,7 @@
 import server from "..";
 
 server
-  .listen(process.env.PORT || 3000)
+  .listen(process.env.PORT || 3000, process.env.ADDRESS || "127.0.0.1")
   .then(() => log("server", "started listening for requests"));
 
 declare global {
@@ -9,6 +9,9 @@ declare global {
     interface ProcessEnv {
       /** The port to serve the site on. */
       PORT?: string;
+
+      /** The address to serve the site on. */
+      ADDRESS?: string;
     }
   }
 }
