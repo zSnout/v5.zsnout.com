4758043f92061f7dc261b2960f9f451150457920
Zachary <zsakowitz@zsnout.com>
2021-12-31 15:14:55 -0800

    Add cdnjs to style-src

--- server/fastify/static.ts
+++ server/fastify/static.ts
@@ -5,7 +5,7 @@ import server from "..";
 
 let csp = `default-src 'self'
   connect-src 'self' blob: https://fonts.googleapis.com/ https://fonts.gstatic.com/ https://cdnjs.cloudflare.com/
-  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com/
+  style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com/ https://fonts.googleapis.com/
   script-src 'self' https://cdnjs.cloudflare.com/
   img-src 'self' data: blob:
   font-src 'self' https://fonts.gstatic.com/
