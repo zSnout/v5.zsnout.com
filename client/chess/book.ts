559d1211fc04502bfb752faaf1161d4ca38a6cb7
Zachary <zsakowitz@zsnout.com>
2022-01-06 09:22:13 -0800

    Make `book.ts` randomly select moves

--- client/chess/book.ts
+++ client/chess/book.ts
@@ -1,4 +1,4 @@
-import type { ChessInstance, ShortMove } from "chess.js";
+import type { ChessInstance } from "chess.js";
 
 /** A promise resolving to a list of grandmaster games. */
 export let book = fetch("/chess/book.txt")
@@ -13,8 +13,15 @@ export let book = fetch("/chess/book.txt")
 export default async function nextGMMove(game: ChessInstance) {
   let allGames = await book;
   let pgn = game.pgn().replace(/\d+\. /g, "");
-  let games = allGames.filter((e) => e.startsWith(pgn));
-  if (!games.length) return null;
-  let line = games[Math.floor(Math.random() * games.length)];
-  return line.slice(pgn.length + 1).split(" ")[0] || null;
+  let moves = [
+    ...new Set(
+      allGames
+        .filter((e) => e.startsWith(pgn))
+        .map((e) => e.slice(pgn.length))
+        .map((e) => e.split(" ")[0] || e.split(" ")[1])
+        .filter((e) => e != "0-1" && e != "1-0" && e != "1/2-1/2" && e)
+    ),
+  ];
+  if (!moves.length) return null;
+  return moves[Math.floor(Math.random() * moves.length)];
 }
