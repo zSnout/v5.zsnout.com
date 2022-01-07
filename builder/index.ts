6bee1bbe2fe8065a7b08834ebc416fac76175936
Zachary <zsakowitz@zsnout.com>
2022-01-07 08:59:31 -0800

    Update folder locations

--- builder/index.ts
+++ builder/index.ts
@@ -5,11 +5,11 @@ import { buildDir as buildMD, watchDir as watchMD } from "./md";
 import { buildDir as buildJS, watchDir as watchJS } from "./js";
 
 if (process.argv[2] == "-b") {
-  buildEJS(join(__dirname, "../../client/"));
-  buildMD(join(__dirname, "../../client/"));
-  buildJS(join(__dirname, "../../.client/"));
+  buildEJS(join(__dirname, "../client/"));
+  buildMD(join(__dirname, "../client/"));
+  buildJS(join(__dirname, "../.client/"));
 } else if (process.argv[2] == "-w") {
-  watchEJS(join(__dirname, "../../client/"));
-  watchMD(join(__dirname, "../../client/"));
-  watchJS(join(__dirname, "../../.client/"));
+  watchEJS(join(__dirname, "../client/"));
+  watchMD(join(__dirname, "../client/"));
+  watchJS(join(__dirname, "../.client/"));
 } else log("cli", "invalid build type");
