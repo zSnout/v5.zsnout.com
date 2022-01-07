f0ae273913c46434806e5905331884ec820ad7e8
Zachary <zsakowitz@zsnout.com>
2022-01-07 10:55:15 -0800

    Add reasoning about (window as any).exports

--- client/chess/chessjs.ts
+++ client/chess/chessjs.ts
@@ -2,4 +2,6 @@ import type { Chess } from "chess.js";
 import "./preload.js";
 import "https://cdnjs.cloudflare.com/ajax/libs/chess.js/0.12.0/chess.min.js";
 
+// Instead of adding an `exports` variable using declare var,
+// we want to prevent `exports` from being introduced into the global scope in `client`.
 export default (window as any).exports.Chess as typeof Chess;
--- client/chess/preload.ts
+++ client/chess/preload.ts
@@ -1,1 +1,5 @@
+// Instead of adding an `exports` variable using declare var,
+// we want to prevent `exports` from being introduced into the global scope in `client`.
 (window as any).exports = {};
+
+export {};
