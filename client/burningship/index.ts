0ac5a3cbc260727d12010f67d00f3aa4c898bd9a
Zachary <zsakowitz@zsnout.com>
2022-01-09 10:02:57 -0800

    Update fractals to use `fractal.ts`

--- client/burningship/index.ts
+++ client/burningship/index.ts
@@ -1,209 +1,31 @@
-import $ from "../assets/js/jsx.js";
-import {
-  getLocationHash,
-  setLocationHash,
-  shuffle,
-} from "../assets/js/util.js";
+import createFractal from "../assets/js/fractal.js";
 
-let canvas = $("#canvas")[0] as HTMLCanvasElement;
-let context = canvas.getContext("2d")!;
-let canvasSize: 340 | 680 | 1360 | 2720 = 680;
-let maxIterations = 100;
-canvas.width = canvasSize;
-canvas.height = canvasSize;
+createFractal(
+  ({ x, y, maxIterations }) => {
+    let zx = 0;
+    let zy = 0;
+    let iter = 0;
 
-let xStart = -2;
-let xEnd = 1;
-let yStart = -2;
-let yEnd = 1;
+    do {
+      zx = Math.abs(zx);
+      zy = Math.abs(zy);
 
-try {
-  let json = JSON.parse(getLocationHash());
+      let [px, py] = [zx * zx - zy * zy, 2 * zx * zy];
+      [zx, zy] = [px + x, py + y];
+      iter += 1;
+    } while (Math.pow(zx, 2) + Math.pow(zy, 2) <= 4 && iter < maxIterations);
 
-  if (typeof json == "object" && json != null) {
-    xStart = json.xStart || xStart;
-    xEnd = json.xEnd || xEnd;
-    yStart = json.yStart || yStart;
-    yEnd = json.yEnd || yEnd;
-    maxIterations = json.maxIterations || maxIterations;
-    canvasSize = json.canvasSize || canvasSize;
-    canvas.width = canvasSize;
-    canvas.height = canvasSize;
+    let frac = 1 - iter / maxIterations;
+    if (frac) return `hsl(${360 * ((frac * 2) % 1)}, 100%, 50%)`;
+    else return "black";
+  },
+  {
+    title: "Burning Ship",
+    xStart: -2,
+    xEnd: 1,
+    yStart: -2,
+    yEnd: 1,
+    maxIterations: 100,
+    canvasSize: 680,
   }
-} catch {}
-
-/**
- * Returns the number of iterations until the point `z` is unbounded.
- * @param cx The real part of `c`.
- * @param cy The imagninary part of `c`.
- * @returns The number of iterations until `z` escapes the circle of radius 2.
- */
-function iterUntilUnbounded(cx: number, cy: number) {
-  let zx = 0;
-  let zy = 0;
-  let iter = 0;
-
-  do {
-    zx = Math.abs(zx);
-    zy = Math.abs(zy);
-
-    let [px, py] = [zx * zx - zy * zy, 2 * zx * zy];
-    [zx, zy] = [px + cx, py + cy];
-    iter += 1;
-  } while (Math.pow(zx, 2) + Math.pow(zy, 2) <= 4 && iter < maxIterations);
-
-  return iter;
-}
-
-let drawID = 0;
-
-/**
- * Redraws the Burning Ship.
- * @param size The size of the fractal to draw.
- * @returns A promise resolving to a boolean indicating whether the fractal was drawn without being canceled.
- */
-async function drawOneBurningShip(size: number = canvasSize) {
-  let myID = (drawID = Math.random());
-  let cxs = shuffle(Array.from({ length: size }, (_, i) => i));
-  let c = 0;
-  let ratio = canvasSize / size;
-
-  for (let i of cxs) {
-    c++;
-    for (let j = 0; j < size; j++) {
-      let cx = xStart + ((xEnd - xStart) * i) / size;
-      let cy = yStart + ((yEnd - yStart) * j) / size;
-
-      let iter = iterUntilUnbounded(cx, cy);
-      let frac = 1 - iter / maxIterations;
-      if (frac) context.fillStyle = `hsl(${360 * ((frac * 2) % 1)}, 100%, 50%)`;
-      else context.fillStyle = "black";
-      context.fillRect(i * ratio, j * ratio, ratio, ratio);
-    }
-
-    if (c % 10 == 0) {
-      await new Promise((resolve) => setTimeout(resolve, 0));
-      if (drawID != myID) return false;
-    }
-  }
-
-  return true;
-}
-
-/**
- * Draws the Burning Ship.
- * @returns A promise that resolves to a boolean indicating whether all fractals were successfully drawn.
- */
-async function drawBurningShip() {
-  let success = await drawOneBurningShip(340);
-  if (success && canvasSize > 340) success = await drawOneBurningShip(680);
-  if (success && canvasSize > 680) success = await drawOneBurningShip(1360);
-  if (success && canvasSize > 1360) success = await drawOneBurningShip(2720);
-  return success;
-}
-
-/**
- * Zooms in on a point in the Burning Ship with 2x zoom.
- * @param cx The real part of the point to zoom in on.
- * @param cy The imaginary part of the point to zoom in on.
- */
-function zoomIn(cx: number, cy: number) {
-  let xDelta = (xEnd - xStart) / 4;
-  let yDelta = (yEnd - yStart) / 4;
-
-  xStart = cx - xDelta;
-  xEnd = cx + xDelta;
-  yStart = cy - yDelta;
-  yEnd = cy + yDelta;
-}
-
-/**
- * Zooms in on a point in the Burning Ship with 2x zoom.
- * @param cx The real part of the point to zoom in on.
- * @param cy The imaginary part of the point to zoom in on.
- */
-function zoomOut(cx: number, cy: number) {
-  let xDelta = xEnd - xStart;
-  let yDelta = yEnd - yStart;
-
-  xStart = cx - xDelta;
-  xEnd = cx + xDelta;
-  yStart = cy - yDelta;
-  yEnd = cy + yDelta;
-}
-
-/** Sets the page hash to match the current settings. */
-function setPageHash() {
-  setLocationHash(
-    JSON.stringify({ xStart, xEnd, yStart, yEnd, maxIterations, canvasSize })
-  );
-}
-
-/** Sets the page title according to the resolution. */
-function setPageTitle() {
-  document.title = `Burning Ship (${canvasSize / 680}x Resolution)`;
-}
-
-setPageTitle();
-setPageHash();
-drawBurningShip();
-
-$("#canvas").on("click", ({ target, clientX, clientY, shiftKey }) => {
-  let { left, top, height, width } = target.getBoundingClientRect();
-  let x = clientX - left;
-  let y = clientY - top;
-  let cx = xStart + ((xEnd - xStart) * x) / width;
-  let cy = yStart + ((yEnd - yStart) * y) / height;
-
-  if (shiftKey) zoomOut(cx, cy);
-  else zoomIn(cx, cy);
-  setPageHash();
-  drawBurningShip();
-});
-
-$("#icon-blur").on("click", () => {
-  if (maxIterations <= 5) maxIterations = 5;
-  else if (maxIterations <= 25) maxIterations -= 5;
-  else if (maxIterations <= 50) maxIterations = 25;
-  else maxIterations -= 50;
-
-  setPageHash();
-  drawBurningShip();
-});
-
-$("#icon-focus").on("click", () => {
-  if (maxIterations < 5) maxIterations = 5;
-  else if (maxIterations < 25) maxIterations += 5;
-  else if (maxIterations < 50) maxIterations = 50;
-  else maxIterations += 50;
-
-  setPageHash();
-  drawBurningShip();
-});
-
-$("#icon-resolution").on("click", () => {
-  if (canvasSize == 340) canvasSize = 680;
-  else if (canvasSize == 680) canvasSize = 1360;
-  else if (canvasSize == 1360) canvasSize = 2720;
-  else canvasSize = 340;
-  canvas.width = canvasSize;
-  canvas.height = canvasSize;
-
-  setPageTitle();
-  setPageHash();
-  drawBurningShip();
-});
-
-$("#icon-zoomin").on("click", () => {
-  zoomIn((xStart + xEnd) / 2, (yStart + yEnd) / 2);
-  setPageHash();
-  drawBurningShip();
-});
-
-$("#icon-zoomout").on("click", () => {
-  zoomOut((xStart + xEnd) / 2, (yStart + yEnd) / 2);
-  setPageHash();
-  drawBurningShip();
-});
-
-$("#canvas").autoResize();
+);
--- client/newton/index.ts
+++ client/newton/index.ts
@@ -1,7 +1,3 @@
-// I am aware that "Newton's Fractal" is really a set of fractals.
-// I am also aware that "Newton's Fractal" is not the real name for this phenomenon.
-// To simplify things, we only plot one fractal.
-
 import {
   add,
   angle,
@@ -12,171 +8,23 @@ import {
   sqr,
   sub,
 } from "../assets/js/complex.js";
-import $ from "../assets/js/jsx.js";
-import {
-  getLocationHash,
-  setLocationHash,
-  shuffle,
-} from "../assets/js/util.js";
+import createFractal from "../assets/js/fractal.js";
 
-let canvas = $("#canvas")[0] as HTMLCanvasElement;
-let context = canvas.getContext("2d")!;
-let canvasSize: 340 | 680 | 1360 | 2720 = 680;
-let maxIterations = 20;
-canvas.width = canvasSize;
-canvas.height = canvasSize;
-
-let xStart = -1;
-let xEnd = 1;
-let yStart = -1;
-let yEnd = 1;
-
-try {
-  let json = JSON.parse(getLocationHash());
-
-  if (typeof json == "object" && json != null) {
-    xStart = json.xStart || xStart;
-    xEnd = json.xEnd || xEnd;
-    yStart = json.yStart || yStart;
-    yEnd = json.yEnd || yEnd;
-    maxIterations = json.maxIterations || maxIterations;
-    canvasSize = json.canvasSize || canvasSize;
-    canvas.width = canvasSize;
-    canvas.height = canvasSize;
-  }
-} catch {}
-
-function finalIteration(z: Complex): Complex {
-  for (let i = 0; i < maxIterations; i++) {
-    z = sub(z, div(add([-1, 0], cube(z)), mult([3, 0], sqr(z))));
-  }
-
-  return z;
-}
-
-function colorOfRootNearest(z: Complex) {
-  let final = finalIteration(z);
-  return `hsl(${(360 * angle(final)) / Math.PI}, 100%, 50%)`;
-}
-
-let drawID = 0;
-
-/** Redraws a fractal using Newton's Method. */
-async function drawNewtonsFractal() {
-  let myID = (drawID = Math.random());
-  let cxs = shuffle(Array.from({ length: canvasSize }, (_, i) => i));
-
-  let c = 0;
-
-  for (let i of cxs) {
-    c++;
-    for (let j = 0; j < canvasSize; j++) {
-      let cx = xStart + ((xEnd - xStart) * i) / canvasSize;
-      let cy = yStart + ((yEnd - yStart) * j) / canvasSize;
-
-      let color = colorOfRootNearest([cx, cy]);
-      context.fillStyle = color;
-      context.fillRect(i, j, 1, 1);
+createFractal(
+  ({ x, y, maxIterations }) => {
+    let z: Complex = [x, y];
+    for (let i = 0; i < maxIterations; i++) {
+      z = sub(z, div(add([-1, 0], cube(z)), mult([3, 0], sqr(z))));
     }
 
-    if (c % 10 == 0) {
-      await new Promise((resolve) => setTimeout(resolve, 0));
-      if (drawID != myID) return;
-    }
+    return `hsl(${(360 * angle(z)) / Math.PI}, 100%, 50%)`;
+  },
+  {
+    title: "Newton's Fractal",
+    xStart: -3,
+    xEnd: 3,
+    yStart: -3,
+    yEnd: 3,
+    maxIterations: 10,
   }
-}
-
-/**
- * Zooms in on a point in the Mandlebrot set with 2x zoom.
- * @param cx The real part of the point to zoom in on.
- * @param cy The imaginary part of the point to zoom in on.
- */
-function zoomIn(cx: number, cy: number) {
-  let xDelta = (xEnd - xStart) / 4;
-  let yDelta = (yEnd - yStart) / 4;
-
-  xStart = cx - xDelta;
-  xEnd = cx + xDelta;
-  yStart = cy - yDelta;
-  yEnd = cy + yDelta;
-}
-
-/**
- * Zooms in on a point in the Mandlebrot set with 2x zoom.
- * @param cx The real part of the point to zoom in on.
- * @param cy The imaginary part of the point to zoom in on.
- */
-function zoomOut(cx: number, cy: number) {
-  let xDelta = xEnd - xStart;
-  let yDelta = yEnd - yStart;
-
-  xStart = cx - xDelta;
-  xEnd = cx + xDelta;
-  yStart = cy - yDelta;
-  yEnd = cy + yDelta;
-}
-
-/** Sets the page hash to match the current settings. */
-function setPageHash() {
-  setLocationHash(
-    JSON.stringify({ xStart, xEnd, yStart, yEnd, maxIterations, canvasSize })
-  );
-}
-
-setPageHash();
-drawNewtonsFractal();
-
-$("#canvas").on("click", ({ target, clientX, clientY, shiftKey }) => {
-  let { left, top, height, width } = target.getBoundingClientRect();
-  let x = clientX - left;
-  let y = clientY - top;
-  let cx = xStart + ((xEnd - xStart) * x) / width;
-  let cy = yStart + ((yEnd - yStart) * y) / height;
-
-  if (shiftKey) zoomOut(cx, cy);
-  else zoomIn(cx, cy);
-  setPageHash();
-  drawNewtonsFractal();
-});
-
-$("#icon-blur").on("click", () => {
-  if (maxIterations <= 10) maxIterations = 5;
-  else maxIterations -= 10;
-
-  setPageHash();
-  drawNewtonsFractal();
-});
-
-$("#icon-focus").on("click", () => {
-  if (maxIterations < 10) maxIterations = 10;
-  else maxIterations += 10;
-
-  setPageHash();
-  drawNewtonsFractal();
-});
-
-$("#icon-resolution").on("click", () => {
-  if (canvasSize == 340) canvasSize = 680;
-  else if (canvasSize == 680) canvasSize = 1360;
-  else if (canvasSize == 1360) canvasSize = 2720;
-  else canvasSize = 340;
-  canvas.width = canvasSize;
-  canvas.height = canvasSize;
-
-  setPageHash();
-  drawNewtonsFractal();
-});
-
-$("#icon-zoomin").on("click", () => {
-  zoomIn((xStart + xEnd) / 2, (yStart + yEnd) / 2);
-  setPageHash();
-  drawNewtonsFractal();
-});
-
-$("#icon-zoomout").on("click", () => {
-  zoomOut((xStart + xEnd) / 2, (yStart + yEnd) / 2);
-  setPageHash();
-  drawNewtonsFractal();
-});
-
-$("#canvas").autoResize();
+);
