c22bde9b500c59b7a441ed651fa0e3fe8839eb28
Zachary <zsakowitz@zsnout.com>
2022-01-12 14:39:19 -0800

    Add overcolorify

--- client/assets/js/png.tsx
+++ client/assets/js/png.tsx
@@ -1,5 +1,6 @@
 import "https://unpkg.com/pngjs@6.0.0/browser.js";
 import type { PNG as PNGInst } from "pngjs";
+import { hslToRgb, rgbToHsl } from "./color.js";
 import $, { jsx } from "./jsx.js";
 
 /** The main PNG class. */
@@ -74,4 +75,32 @@ export function imageToPNG(image: Blob) {
       }
     };
   });
+}
+
+/**
+ * Removes all saturation and lightness from a PNG.
+ * @param png The PNG to overcolor.
+ * @returns The modified PNG.
+ */
+export function overcolorify(png: PNGInst) {
+  for (let y = 0; y < png.height; y++) {
+    for (let x = 0; x < png.width; x++) {
+      let idx = (png.width * y + x) << 2;
+
+      let [r, g, b] = [png.data[idx], png.data[idx + 1], png.data[idx + 2]];
+      let [h, s, l] = rgbToHsl(r, g, b);
+      let [r2, g2, b2] = hslToRgb(h, 1, 0.5);
+
+      if (Math.max(r, g, b) - Math.min(r, g, b) < 8) {
+        let avg = (r + g + b) / 3;
+        r2 = g2 = b2 = avg;
+      }
+
+      png.data[idx] = r2;
+      png.data[idx + 1] = g2;
+      png.data[idx + 2] = b2;
+    }
+  }
+
+  return png;
 }
