58e56bce6beb69553c46aab7c5b5f5a57e1739fa
Zachary <zsakowitz@zsnout.com>
2022-01-16 07:21:55 -0800

    Update bounds

--- client/mandelbrot/index.ts
+++ client/mandelbrot/index.ts
@@ -4,10 +4,10 @@ createMandelbrotLike(
   ([zx, zy], [cx, cy]) => [zx * zx - zy * zy + cx, 2 * zx * zy + cy],
   {
     title: "Mandelbrot Set",
-    xStart: -1.5,
-    xEnd: 0.5,
-    yStart: -1,
-    yEnd: 1,
+    xStart: -2,
+    xEnd: 1,
+    yStart: -1.5,
+    yEnd: 1.5,
     maxIterations: 150,
     canvasSize: 680,
   }
