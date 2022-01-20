0eac433e039f41d55e06c2cea5684c7413f62b47
Zachary <zsakowitz@zsnout.com>
2022-01-20 09:45:17 -0800

    Add util.createGen

--- client/assets/js/util.ts
+++ client/assets/js/util.ts
@@ -180,6 +180,20 @@ export function range(length = 1) {
     .map((_, i) => i);
 }
 
+/**
+ * Creates a random number generator.
+ * @param seed The seed to use.
+ * @returns A random number generator.
+ */
+export function createGen(seed: number) {
+  return () => {
+    let t = (seed += 0x6d2b79f5);
+    t = Math.imul(t ^ (t >>> 15), t | 1);
+    t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
+    return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
+  };
+}
+
 declare global {
   /** A list of items that can be put into localStorage. */
   interface StorageItems {}
