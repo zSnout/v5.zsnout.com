03ee0ca203ff0b802769671d0ffbda8f4d168ba3
Zachary <zsakowitz@zsnout.com>
2022-01-29 20:49:30 -0800

    Remove Desmos page; it's useless

--- client/desmos/index.tsx
+++
@@ -1,7 +0,0 @@
-import $, { jsx } from "../assets/js/jsx.js";
-
-let calc = Desmos.GraphingCalculator($.main[0], { border: false });
-
-$(".dcg-exppanel-container.dcg-add-shadow").prepend(
-  <div className="dcg-padder" />
-);
--- server/fastify/static.ts
+++ server/fastify/static.ts
@@ -4,23 +4,18 @@ import { join } from "path";
 import server from "..";
 
 let csp = `default-src 'self'
-  connect-src 'self' blob: https://chessboardjs.com/ https://fonts.googleapis.com/ https://fonts.gstatic.com/ https://cdnjs.cloudflare.com/ https://unpkg.com/ https://raw.githubusercontent.com/ https://0.peerjs.com/ wss://0.peerjs.com/ https://www.desmos.com/
+  connect-src 'self' blob: https://chessboardjs.com/ https://fonts.googleapis.com/ https://fonts.gstatic.com/ https://cdnjs.cloudflare.com/ https://unpkg.com/ https://raw.githubusercontent.com/ https://0.peerjs.com/ wss://0.peerjs.com/
   style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com/ https://fonts.googleapis.com/
-  script-src 'self' https://cdnjs.cloudflare.com/ https://unpkg.com/ https://www.desmos.com/
+  script-src 'self' https://cdnjs.cloudflare.com/ https://unpkg.com/
   img-src 'self' data: blob: https://chessboardjs.com/
-  font-src 'self' data: https://fonts.gstatic.com/
+  font-src 'self' https://fonts.gstatic.com/
   object-src 'none'
   worker-src 'self' blob: https://unpkg.com/
   base-uri 'none'
   report-uri /csp`.replaceAll("\n", "; ");
 
-server.addHook("preHandler", (req, res, next) => {
+server.addHook("preHandler", (_, res, next) => {
   res.header("content-security-policy", csp);
-  if (req.url.startsWith("/desmos/"))
-    res.header(
-      "content-security-policy",
-      csp.replace("script-src", "script-src 'unsafe-eval'")
-    );
   next();
 });
 
