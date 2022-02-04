0f18bf4168b913dfee9e42a87b693abfe3513900
Zachary <zsakowitz@zsnout.com>
2022-02-03 20:31:08 -0800

    Add more presets

--- client/assets/js/webgl.ts
+++ client/assets/js/webgl.ts
@@ -6,8 +6,11 @@ import { getLocationHash, setLocationHash } from "./util.js";
 let eqMap: Record<string, string> = {
   "z^2+c": "Mandelbrot Set",
   "z^3+c": "Multibrot Set",
+  "z^2+1/c": "Droplet",
   "z^3-z^2-z-c": "Snowflake",
   "(z^3+1)/(cz^2+1)": "Infinite Puddles",
+  "z^2+z+1/c": "Telescope",
+  "z^2-z+1/c": "Rays",
 };
 
 /**
