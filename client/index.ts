0e3493363704707305cd1e4bdba2f51efdc34790
Zachary <zsakowitz@zsnout.com>
2022-02-08 10:45:40 -0800

    Remove theme logic

--- client/index.ts
+++ client/index.ts
@@ -2,30 +2,6 @@ import fetch from "./assets/js/fetch.js";
 import $ from "./assets/js/jsx.js";
 import { getStorage, onStorageChange, setStorage } from "./assets/js/util.js";
 
-/**
- * Sets the theme of the page and calls `checkTheme`.
- * @param theme The theme to set `localStorage` to.
- */
-function setTheme(theme: StorageItems["options:theme"]) {
-  setStorage("options:theme", theme);
-}
-
-$('[href="#theme-aqua"]')
-  .attr("href", "")
-  .on("click", (e) => (e.preventDefault(), setTheme("aqua")));
-
-$('[href="#theme-light"]')
-  .attr("href", "")
-  .on("click", (e) => (e.preventDefault(), setTheme("light")));
-
-$('[href="#theme-dark"]')
-  .attr("href", "")
-  .on("click", (e) => (e.preventDefault(), setTheme("dark")));
-
-$('[href="#theme-yellow-pink"]')
-  .attr("href", "")
-  .on("click", (e) => (e.preventDefault(), setTheme("yellow-pink")));
-
 let auth = getStorage("options:authToken");
 
 if (auth)
@@ -51,17 +27,49 @@ if (auth)
     }
   });
 
-$('[href="#clear-cache"')
+let clearCachePrompt =
+  "Are you sure you want to clear the cache? This will remove offline capabilities of any pages you have visited. To re-enable offline mode, simply visit a page.";
+
+let clearCacheTextEl = $('[href="#clear-cache"] strong');
+let clearCache = $('[href="#clear-cache"]')
   .attr("href", "")
-  .on(
-    "click",
-    async (e) => (
-      e.preventDefault(),
-      confirm(
-        "Are you sure you want to clear the cache? This will remove offline capabilities of any pages you have visited. To re-enable offline mode, simply visit a page."
-      ) && (await caches.keys()).map(caches.delete.bind(caches))
-    )
-  );
+  .on("click", async (e) => {
+    e.preventDefault();
+    if (!confirm(clearCachePrompt)) return;
+    (await caches.keys()).map(caches.delete.bind(caches));
+  });
+
+/**
+ * Gets data from a cache.
+ * @param key The cache key.
+ * @returns A list of response sizes.
+ */
+async function getCacheData(key: string) {
+  let cache = await caches.open(key);
+  let keys = await cache.keys();
+  let resps = await Promise.all(keys.map((key) => cache.match(key)));
+  let all = resps.filter((e): e is Response => !!e);
+  return Promise.all(all.map(async (e) => (await e.blob()).size));
+}
+
+/**
+ * Gets data from all caches.
+ * @returns A list of cached response sizes.
+ */
+async function getAllCacheData() {
+  return caches
+    .keys()
+    .then((keys) => Promise.all(keys.map(getCacheData)))
+    .then((data) => data.flat());
+}
+
+(async () => {
+  let size = (await getAllCacheData()).reduce((a, b) => a + b, 0);
+  let text = `${size} bytes`;
+  if (size > 1024) text = `${(size / 1024).toFixed(2)} KB`;
+  if (size > 1024 * 1024) text = `${(size / 1024 / 1024).toFixed(2)} MB`;
+  clearCacheTextEl.text(`Clear ${text}`);
+})();
 
 /** Adds the current icon size onto the `main` element. */
 function onResize(status = getStorage("options:iconSize")) {
