3e2e0bb8dfa24df59bc0c90e2fb32741407e0c60
Zachary <zsakowitz@zsnout.com>
2022-02-09 07:59:21 -0800

    Fix blob simulator. Fixes blob simulator #5

--- client/blobs/index.ts
+++ client/blobs/index.ts
@@ -1,90 +1,19 @@
-import $ from "../assets/js/jsx.js";
-import { getLocationHash, setLocationHash } from "../assets/js/util.js";
+import "./preload.js";
+import "https://unpkg.com/metaballs-js@2.0.0/dist/index.js";
+import { getTheme, ThemeColor } from "../assets/js/util.js";
 
-let canvas = $("#canvas")[0] as HTMLCanvasElement;
-let context = canvas.getContext("2d")!;
-let canvasSize = 1;
-let blobEndChance = 0.8;
-canvas.width = canvasSize;
-canvas.height = canvasSize;
+let bg = getTheme(ThemeColor.BackgroundColor).trim();
+let text = getTheme(ThemeColor.TextColor).trim();
+if (bg.length == 4) bg += bg.slice(1);
+if (text.length == 4) text += text.slice(1);
 
-try {
-  let json = JSON.parse(getLocationHash());
-
-  if (typeof json == "object" && json != null) {
-    blobEndChance = json.blobEndChance ?? blobEndChance;
-    canvasSize = json.canvasSize || canvasSize;
-    canvas.width = canvasSize;
-    canvas.height = canvasSize;
-  }
-} catch {}
-
-/** Sets the page hash to match the current settings. */
-function setPageHash() {
-  setLocationHash(JSON.stringify({ blobEndChance, canvasSize }));
-}
-
-/** A list of coordinates that have been visited. */
-let coords = new Set<string>();
-
-/**
- * Draws a blob.
- * @param x The x coordinate of the center of the blob.
- * @param y The y coordinate of the center of the blob.
- */
-function drawBlob(x: number, y: number) {
-  coords.add(`${x},${y}`);
-  context.fillRect(x, y, 1, 1);
-
-  if (Math.random() < blobEndChance) return;
-
-  if (!coords.has(`${x - 1},${y}`) && x - 1 > 0) drawBlob(x - 1, y);
-  if (!coords.has(`${x + 1},${y}`) && x + 1 < canvasSize) drawBlob(x + 1, y);
-  if (!coords.has(`${x},${y - 1}`) && y - 1 > 0) drawBlob(x, y - 1);
-  if (!coords.has(`${x},${y + 1}`) && y + 1 < canvasSize) drawBlob(x, y + 1);
-}
-
-/** Redraws the main canvas. */
-function drawImage() {
-  context.fillStyle = "white";
-  context.fillRect(0, 0, canvas.width, canvas.height);
-  context.fillStyle = "black";
-  drawBlob(canvasSize / 2, canvasSize / 2);
-}
-
-$("#icon-smallblob").on("click", () => {
-  if (blobEndChance >= 0.95) blobEndChance = 1;
-  else blobEndChance += 0.05;
-
-  setPageHash();
-  drawImage();
+let initMetaballs = (window as any).module.exports;
+initMetaballs("#canvas", {
+  backgroundColor: bg,
+  color: text,
+  minRadius: 2,
+  maxRadius: 5,
+  numMetaballs: 75,
+  speed: 10,
+  interactive: "canvas",
 });
-
-$("#icon-bigblob").on("click", () => {
-  if (blobEndChance <= 0.05) blobEndChance = 0;
-  else blobEndChance -= 0.05;
-
-  setPageHash();
-  drawImage();
-});
-
-$("#icon-resolution").on("click", () => {
-  canvasSize *= 2;
-  if (canvasSize >= 8) canvasSize = 1;
-
-  onResize();
-  setPageHash();
-  drawImage();
-});
-
-function onResize() {
-  $.main.empty();
-  canvas.width = ($.main.width() * canvasSize) / 2;
-  canvas.height = ($.main.height() * canvasSize) / 2;
-  $.main.append(canvas);
-}
-
-window.addEventListener("resize", onResize);
-onResize();
-drawImage();
-$("#canvas").on("click", drawImage);
